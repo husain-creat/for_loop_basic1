b8a973a8ddb27080cec1caeb586e6e597442bb09
Husain Draidi <husaindradi@gmail.com>
2023-05-07 11:58:06 +0300

    loops in python

---
+++ for_loop_basic1.py
@@ -0,0 +1,23 @@
+#for i in range(0,51,1):
+    #print(i)
+#for i in range(5,1000,1):
+    #if i%5==0:
+        #print(i)
+#for i in range(1,101,1):
+   # if i%5==0 and i%10 !=0:
+        #print("coding")
+    #elif i%10==0:
+        #print("coding dojo")
+"""  
+sum = 0
+for x in range(1,500001,1):
+    sum+=x 
+print(sum)
+"""
+hig_num =50
+low_num =8
+mult = 6
+for i in range(low_num,hig_num,1):
+    if i%mult==0:
+        print(i)      
+
